1aed04ccbe1886a29bf6976c25cb4099ca5374cd
superCB <1956699404@qq.com>
2020-10-16 00:33:47 +0800

    init the project

---
+++ src/main/java/HomeWork.java
@@ -0,0 +1,9 @@
+import org.antlr.v4.runtime.ANTLRInputStream;
+
+public class HomeWork   {
+
+    public static void main(String[] args) {
+//        Math\
+        ANTLRInputStream in = new ANTLRInputStream();
+    }
+}
