5de86c55c70052338d606f8a8e26bc1cf8f88992
superCB <1956699404@qq.com>
2020-10-21 20:51:03 +0800

    基本完成了词法分析器的任务。语法文件需要继续调整 文本框需要加上滑动轮。

--- src/main/java/CompilerManager.java
+++ src/main/java/CompilerManager.java
@@ -1,42 +1,33 @@
 
 import FileOperation.MyFileManager;
-import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.*;
 
-import org.antlr.v4.runtime.CommonToken;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.util.List;
 import java.util.Queue;
 
-public class HomeWork {
+public class CompilerManager {
 
     public static void run(String str)  {
         ANTLRInputStream inputStream = new ANTLRInputStream(str);
         CBLexer lexer = new CBLexer(inputStream);
-        String[] t = lexer.getTokenNames();
 
 
         List<CommonToken> c2 = (List<CommonToken>) lexer.getAllTokens();
-        List<? extends Token> c = lexer.getAllTokens();
-        System.out.println("列表的长度为 " + c.size());
         for (CommonToken c1 : c2) {
             System.out.println(c1.getText());
             System.out.println("第" + c1.getLine() + "行");
             System.out.println("起始位置" + c1.getStartIndex());
-//            c1.getLine()+c1.getStartIndex()+c1.getStopIndex();
         }
 
 
     }
 
     public static void main(String[] args) throws Exception {
-//        Math\
         String[] testStr = {
                 "a=3     ;",
                 "float b=4;",
-                // "hi world"
         };
 
         MyFileManager myFileManager = new MyFileManager();
@@ -45,7 +36,5 @@ public class HomeWork {
             run(c);
             break;
         }
-//        Queue<String>
-
     }
 }
--- src/main/java/GUI/CBFrame.java
+++ src/main/java/GUI/CBFrame.java
@@ -1,40 +1,79 @@
 package GUI;
 
+import MyAntlr.CBLexer;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonToken;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
-//import
+
+import javax.swing.filechooser.FileFilter;
+
+/**
+ * @author 全栈工程师崔博先生
+ */
 public class CBFrame extends JFrame {
 
     //    private final Object ActionListener;
 //    MyButton open1 = null;
     private int Width;
-    private int Height;
-
+    private int height;
+    private FileFilter myfileFilter;
+    private JTextArea sourceCFile, afterLexing;
+    private LinkedList<String> lines;
+    private boolean choosed;
 
     /**
      * 使用空布局的方法进行布局
      *
-     *
+     * @throws HeadlessException
      */
     private void initButtons() {
         JButton chooseFile = new JButton("选择C语言文件");
-        chooseFile.setBounds(Width / 32,  Height / 5, Width / 8, Height / 10);
+        chooseFile.setBounds(Width / 32, height / 5, Width / 8, height / 10);
+
+
         chooseFile.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                fileChooser();
+//                fileChooser();
+                lines = getFileLines();
+                if (lines == null) {
+                } else {
+                    for (String t : lines) {
+                        sourceCFile.append(t + '\n');
+                    }
+                    choosed = true;
+                }
             }
         });
         JButton lexerButton = new JButton("词法分析");
-        lexerButton.setBounds(Width / 32, 3 * Height / 5, Width / 8, Height / 10);
+        lexerButton.setBounds(Width / 32, 3 * height / 5, Width / 8, height / 10);
         lexerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                if (lines != null) {
+                    for (String str : lines) {
+                        ANTLRInputStream inputStream = new ANTLRInputStream(str);
+                        CBLexer lexer = new CBLexer(inputStream);
+                        java.util.List<CommonToken> c2 = (List<CommonToken>) lexer.getAllTokens();
+                        for (CommonToken c1 : c2) {
+                            String t = "";
+                            t += c1.getText()+" ";
+                            t += "第" + c1.getLine() + "行   ";
+                            t += "起始位置" + c1.getStartIndex();
+                            afterLexing.append(t+'\n');
+                        }
+//                        afterLexing.append(str);
+                    }
+                    choosed = false;
+                }
             }
         });
         this.add(lexerButton);
@@ -43,21 +82,55 @@ public class CBFrame extends JFrame {
 
 
     public CBFrame() throws HeadlessException {
-
+        init();
         initBound();
         initButtons();
+        JLabel left = new JLabel("源文件");
+        left.setBounds(Width / 3, height / 10, Width / 6, height / 12);
 
-        JTextField sourceCFile = new JTextField("dasf", 10);
-        sourceCFile.setFont(new Font(null, Font.PLAIN | Font.BOLD | Font.ITALIC, 20));
+//        this.add(left);
+        sourceCFile = new JTextArea(40, 10);
+        sourceCFile.setFont(new Font(null, Font.PLAIN | Font.BOLD, 20));
+        sourceCFile.setLineWrap(true);
+        sourceCFile.setBounds(Width / 6, height / 10, Width / 3, 3 * height / 4);
+        afterLexing = new JTextArea(40, 10);
 
-        sourceCFile.setBounds(Width / 6, Height / 15, Width / 3, 3 * Height / 4);
-        this.setLayout(null);
-
+        afterLexing.setFont(new Font(null, Font.PLAIN | Font.BOLD, 20));
+        afterLexing.setBounds(Width / 2 + Width / 12, height / 10, Width / 3, 3 * height / 4);
 
         this.add(sourceCFile);
+        this.add(afterLexing);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    }
+
+    /**
+     * 文件过滤器只有以.c结尾的文件才能够被接受
+     */
+    private void init() {
+//设置一个文件过滤器
+        this.setTitle("简单C语言词法分析器");
+        this.setLayout(null);
+        this.setResizable(false);//窗口的大小不可更改
+        myfileFilter = new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                if (f.getName().endsWith(".c")) {
+                    return true;
+                } else {
+                    return false;
+                }
+
+            }
+
+            ;
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+        };
     }
 
     /**
@@ -68,20 +141,66 @@ public class CBFrame extends JFrame {
         Dimension dimension = kit.getScreenSize();
 
         Width = dimension.width;
-        Height = dimension.height;
-        setBounds(Width / 4, Height / 4, Width / 2, Height / 2);
+        height = dimension.height;
+        setBounds(Width / 4, height / 4, Width / 2, height / 2);
 
         Width = dimension.width / 2;
-        Height = dimension.height / 2;
+        height = dimension.height / 2;
     }
 
+    /**
+     * 初始化一个文件选择器，并返回文件内所有字符串集组成的集合
+     *
+     * @return c语言源文件字符串集合
+     */
+    private LinkedList<String> getFileLines() {
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+
+        jFileChooser.setFileFilter(myfileFilter);
+        jFileChooser.showDialog(new JLabel(), "选择一个C语言文件");
+        LinkedList<String> stringLinkedList = new LinkedList<>();
+
+        File file = jFileChooser.getSelectedFile();
+        if (file == null) {
+            return null;
+        }
+        if (file.isFile()) {
+
+
+            try {
+                FileReader reader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String str;
+                while ((str = bufferedReader.readLine()) != null) {//用null来判断就不会出现忽略空格的情况
+//                    System.out.println(str);
+                    stringLinkedList.add(str);
+                }
+
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+        if (stringLinkedList.isEmpty()) {
+            return null;
+        }
+        return stringLinkedList;
+    }
 
     public void fileChooser() {
         JFileChooser jFileChooser = new JFileChooser();
         jFileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-        jFileChooser.showDialog(new JLabel(), "选择一个C语言文件");
-        File file = jFileChooser.getSelectedFile();
 
+
+        jFileChooser.setFileFilter(myfileFilter);
+        jFileChooser.showDialog(new JLabel(), "选择一个C语言文件");
+
+
+        File file = jFileChooser.getSelectedFile();
+        if (file == null) {
+            return;
+        }
         if (file.isDirectory()) {
 
             System.out.println("目录");
@@ -93,11 +212,13 @@ public class CBFrame extends JFrame {
                 FileReader reader = new FileReader(file);
                 BufferedReader bufferedReader = new BufferedReader(reader);
                 String t;
-                while ((t = bufferedReader.readLine()).length() > 0) {
+                while ((t = bufferedReader.readLine()) != null) {
                     System.out.println(t);
                 }
-            } catch (IOException fileNotFoundException) {
+            } catch (FileNotFoundException fileNotFoundException) {
                 fileNotFoundException.printStackTrace();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
             }
         }
 
